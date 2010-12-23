2a89c5256618b15dc44e511f20841ed829d4a6fd
cjnash <nashcjnash@mac.com>
2010-12-23 12:01:25 -0700

    First commit of content inventory app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaraudersMap::Application.config.secret_token = '4114d03e40ab6ce59bacca6deee8f90949dd61e60ae71f5052ea04f82ff06390c4602054f247351f348100efb2d406ac3c6cffb1b5c387870d83fe6fbdc236d8'
